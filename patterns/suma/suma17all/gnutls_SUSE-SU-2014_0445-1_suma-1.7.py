68d63a73d7f602e778e21cfb52ad44359eb03aea
Jason Record <g23guy@gmail.com>
2014-12-03 15:03:41 -0700

    new patterns

---
+++ patterns/suma/suma17all/gnutls_SUSE-SU-2014_0445-1_suma-1.7.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#
+# Title:       Important SUMA Security Announcement for gnutls SUSE-SU-2014:0445-1
+# Description: Security fixes for SUSE Manager 1.7
+# Source:      SUMA Security Announcement Parser v0.2.5
+# Modified:    2014 Dec 03
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+import suma
+
+META_CLASS = "Security"
+META_CATEGORY = "SUMA"
+META_COMPONENT = "gnutls"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-03/msg00020.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'gnutls'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:0445-1'
+PACKAGES = {}
+SUMA = suma.getSumaInfo()
+
+if ( SUMA['Installed'] ):
+	if ( SUMA['Version'] == '1.7' ):
+		PACKAGES = {
+			'libgnutls26-32bit': '2.4.1-24.39.49.1',
+			'gnutls': '2.4.1-24.39.49.1',
+			'libgnutls26': '2.4.1-24.39.49.1',
+			'libgnutls-extra26': '2.4.1-24.39.49.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the SUMA version scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: SUMA Not Installed")
+Core.printPatternResults()
+
---
+++ patterns/suma/suma21all/spacewalk-java_SUSE-SU-2014_1342-1_suma-2.1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+# Title:       Important SUMA Security Announcement for spacewalk-java SUSE-SU-2014:1342-1
+# Description: Security fixes for SUSE Manager 2.1
+# Source:      SUMA Security Announcement Parser v0.2.5
+# Modified:    2014 Dec 03
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+import suma
+
+META_CLASS = "Security"
+META_CATEGORY = "SUMA"
+META_COMPONENT = "spacewalk-java"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-10/msg00010.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'spacewalk-java'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:1342-1'
+PACKAGES = {}
+SUMA = suma.getSumaInfo()
+
+if ( SUMA['Installed'] ):
+	if ( SUMA['Version'] == '2.1' ):
+		PACKAGES = {
+			'spacewalk-java-oracle': '2.1.165.6-0.13.1',
+			'spacewalk-java-config': '2.1.165.6-0.13.1',
+			'spacewalk-java-lib': '2.1.165.6-0.13.1',
+			'spacewalk-java': '2.1.165.6-0.13.1',
+			'spacewalk-taskomatic': '2.1.165.6-0.13.1',
+			'spacewalk-java-postgresql': '2.1.165.6-0.13.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the SUMA version scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: SUMA Not Installed")
+Core.printPatternResults()
+
